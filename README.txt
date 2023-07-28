17b66c8045c1baca663520d0a4e96bb476769ad5
chris Gong <gongzhen2015@gmail.com>
2023-07-28 18:31:49 -0500

    Third

--- README.txt
+++ README.txt
@@ -2,7 +2,8 @@
 1. Install python-3.11.4-amd64.exe
 2. Open CMD terminal
 3. Type python --version and hit Enter
-4. Verfity it shows Python 3.11.4
+    Put downloaded and extract package under C:\Users\Administrator
+4. Verify it shows Python 3.11.4
 8. type 'cd golf_scheduler'
 9. Type 'pip install -r requirements.txt' wait for installation to get done
 10. Type 'py manage.py runserver'
--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -20,22 +20,21 @@ def job_scheduler_start_now():
                         hour=task_scheduler_obj.Job_1_Schedule_Hour,
                         minute=task_scheduler_obj.Job_1_Schedule_Minute,
                         second='00', id='period_task', replace_existing=True)
-    # if task_scheduler_obj.Job_2_Start:
-    #     scheduler_1.add_job(job_scheduler_start_period, 'cron', day_of_week=task_scheduler_obj.Job_2_Schedule_Day,
-    #                         hour=task_scheduler_obj.Job_2_Schedule_Hour,
-    #                         minute=task_scheduler_obj.Job_2_Schedule_Minute,
-    #                         second='00', id='period_task_2', replace_existing=True)
-    # else:
-    #     try:
-    #         scheduler_1.remove_job('period_task_2')
-    #     except:
-    #         pass
 
 
 def job_scheduler_start_period():
     scheduler_obj = Scheduler.objects.get(user='admin')
-    book_golf(scheduler_obj.Website_Url, scheduler_obj.Username, scheduler_obj.Password,
-              scheduler_obj.Time_Slot, scheduler_obj.Date, scheduler_obj.Email)
+    book_golf(scheduler_obj.Website_Url, scheduler_obj.Username_1, scheduler_obj.Password_1,
+              scheduler_obj.Time_Slot_1, scheduler_obj.Date_1, scheduler_obj.Email)
+    if scheduler_obj.Start_2:
+        book_golf(scheduler_obj.Website_Url, scheduler_obj.Username_1, scheduler_obj.Password_1,
+              scheduler_obj.Time_Slot_2, scheduler_obj.Date_2, scheduler_obj.Email)
+    if scheduler_obj.Start_3:
+        book_golf(scheduler_obj.Website_Url, scheduler_obj.Username_2, scheduler_obj.Password_2,
+                  scheduler_obj.Time_Slot_3, scheduler_obj.Date_3, scheduler_obj.Email)
+    if scheduler_obj.Start_4:
+        book_golf(scheduler_obj.Website_Url, scheduler_obj.Username_2, scheduler_obj.Password_2,
+                  scheduler_obj.Time_Slot_4, scheduler_obj.Date_4, scheduler_obj.Email)
 
 
 scheduler_1.add_job(job_scheduler_start_period, 'cron', day_of_week='mon', hour=2,
